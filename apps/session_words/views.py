6d9209c1fb40e330f9967219f29f99d5f7344d2e
ctdoan33 <doanchristopher_1@yahoo.com>
2017-09-17 16:33:07 -0700

    initial commit

---
+++ apps/session_words/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from time import strftime, localtime
+def index(request):
+    if "words" not in request.session:
+        request.session["words"]=[]
+    return render(request, "session_words/index.html")
+def add_word(request):
+    if "font" in request.POST:
+        font="2em"
+    else:
+        font="1em"
+    allwords = request.session["words"]
+    allwords.append({
+        "word":request.POST["word"],
+        "color":request.POST["color"],
+        "font":font,
+        "time":strftime("%I:%M:%S%p, %B %d, %Y", localtime())
+    })
+    request.session["words"] = allwords
+    return redirect("/session_words")
+def clear(request):
+    request.session.clear()
+    return redirect("/session_words")
